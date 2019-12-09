5e4be33456b9bc9fdb12fcedf485165bbbd65fff
Darshan Parajuli <parajulidarshan@gmail.com>
2019-12-09 15:37:21 -0800

    Fix `get_input`

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 use std::env;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
-use std::io::BufReader;
+use std::io::{self, BufReader};
 
 pub fn read_input() -> Result<Vec<String>, io::Error> {
-    env::args()
+    let arg = env::args()
         .skip(1)
-        .take(1)
-        .map(|arg| BufReader::new(File::open(arg)?).lines().collect())
-        .collect()
+        .next()
+        .ok_or(io::Error::from(io::ErrorKind::InvalidInput))?;
+    BufReader::new(File::open(arg)?).lines().collect()
 }
